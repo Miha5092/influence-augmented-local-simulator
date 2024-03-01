1eed1db6f4df29d5a7c9ff9a70bc9b3b811baea5
Mihai Mitrea <M.Mitrea@student.tudelft.nl>
2024-03-01 14:17:21 +0100

    Skeleton class

---
+++ main.py
@@ -0,0 +1,11 @@
+from OneDimensionalEnvironment import OneDimensionalEnvironment
+
+
+def main():
+    env = OneDimensionalEnvironment()
+
+    print(env.step(0))
+
+
+if __name__ == "__main__":
+    main()
