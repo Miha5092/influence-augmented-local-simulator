496e527a0a74062880a59f2fc967bc924e674260
Mihai Mitrea <M.Mitrea@student.tudelft.nl>
2024-03-13 19:55:04 +0100

    Switched to one dimension

--- src/main.py
+++ src/main.py
@@ -1,11 +1,23 @@
-from src.environment.Environment import Environment
+from one_dimensional.OneDimensionalEnvironment import OneDimensionalEnvironment
+import yaml
 
 
 def main():
-    env = Environment()
-    env.reset()
 
-    print(env)
+    # # Define your tuple
+    # my_tuple = ((0, (0, 1)), (1, (1, 1)))
+    #
+    # # Define the file path
+    # file_path = 'tuple.yaml'
+    #
+    # # Write the tuple to a YAML file
+    # with open(file_path, 'w') as file:
+    #     yaml.dump({'node_name': {
+    #         'value': 1,
+    #         'parents': ['node_name'],
+    #         'CPT': my_tuple
+    #     }}, file)
+    env = OneDimensionalEnvironment()
 
 
 if __name__ == "__main__":
---
+++ src/one_dimensional/cpt.py
@@ -0,0 +1,9 @@
+class CPT:
+    def __init__(self):
+        pass
+
+    def evaluate(self, node_index: int) -> int:
+        pass
+
+    def evaluate(self, indices: tuple[int, int, int]) -> tuple[int, int, int]:
+        pass
