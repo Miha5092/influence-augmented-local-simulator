3f65d0c2883054190e46f3b05995c27c7d67abd1
Mihai Mitrea <M.Mitrea@student.tudelft.nl>
2024-04-17 17:33:36 +0200

    Created network class and changed everything

--- src/main.py
+++ src/main.py
@@ -1,23 +1,12 @@
-from one_dimensional.OneDimensionalEnvironment import OneDimensionalEnvironment
-import yaml
+from src.one_dimensional.WaveWorld import WaveWorld
 
 
 def main():
 
-    # # Define your tuple
-    # my_tuple = ((0, (0, 1)), (1, (1, 1)))
-    #
-    # # Define the file path
-    # file_path = 'tuple.yaml'
-    #
-    # # Write the tuple to a YAML file
-    # with open(file_path, 'w') as file:
-    #     yaml.dump({'node_name': {
-    #         'value': 1,
-    #         'parents': ['node_name'],
-    #         'CPT': my_tuple
-    #     }}, file)
-    env = OneDimensionalEnvironment()
+    wave_world = WaveWorld(world_size=7, local_simulation=False)
+
+    for _ in range(10):
+        wave_world.step(0, verbose=True)
 
 
 if __name__ == "__main__":
